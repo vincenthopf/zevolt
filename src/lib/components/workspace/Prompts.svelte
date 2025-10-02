20072d8ab9a682770192018d4ec76f5663315535
vincenthopf <vincenthopf@users.noreply.github.com>
2025-10-02 17:25:10 +1000

    feat: Rebrand Svelte components UI text from Open WebUI to Embr

--- src/lib/components/workspace/Prompts.svelte
+++ src/lib/components/workspace/Prompts.svelte
@@ -53,7 +53,7 @@
 	});
 
 	const shareHandler = async (prompt) => {
-		toast.success($i18n.t('Redirecting you to Open WebUI Community'));
+		toast.success($i18n.t('Redirecting you to Embr Community'));
 
 		const url = 'https://openwebui.com';
 
@@ -405,7 +405,7 @@
 	{#if $config?.features.enable_community_sharing}
 		<div class=" my-16">
 			<div class=" text-xl font-medium mb-1 line-clamp-1">
-				{$i18n.t('Made by Open WebUI Community')}
+				{$i18n.t('Made by Embr Community')}
 			</div>
 
 			<a
